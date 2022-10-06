a6a16cc457c3d5fc32f25de10555e12ad58836f1
Izmayluv <91906602+Izmayluv@users.noreply.github.com>
2022-10-06 19:54:55 +0300

    testdb

---
+++ TESTDB/TESTDB/Classes/Commands.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TESTDB.Classes
+{
+    internal class Commands
+    {
+
+        public void AuthCommand(string login, string password)
+        {
+            int role = 0;
+            string query = "select * from Customers where userLogin = '" + login + "' and userPassword = '" + password + "'";
+            SqlCommand _sqlCommand = new SqlCommand(query, Connection._connection);
+
+            try
+            {
+                SqlDataReader reader = _sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                   role = reader.GetInt32(6);
+                }
+                
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Пароль верный");
+
+                    if (role.Equals(0))
+                    {
+                        MessageBox.Show("ujdyjjPOFMomF");
+                    }
+
+                    if (role == 1)
+                    {
+                        MessageBox.Show("Вы вошли как клиент");
+                    }
+
+                    if (role == 2)
+                    {
+                        MessageBox.Show("Вы вошли как менеджер");
+                    }
+                    if (role == 3)
+                    {
+                        MessageBox.Show("Вы вошли как администратор");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пароль не верный");
+                }
+                reader.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Выполнить команду не удалось", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
